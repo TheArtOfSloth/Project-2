761a4a902c291d8cef880183da21d4af2d18242a
qk8333 <evan.smith.m@gmail.com>
2018-01-29 18:22:32 -0800

    Update View.h

--- View.h
+++ View.h
@@ -11,16 +11,14 @@ using namespace std;
 #include "Rullo.h"
 
 // L = left, D = down, U = up, R = right, H = highlighted
-enum Unicode {
-	DR = 218, LR = 196, LDR = 194, LD = 191, DU = 179, DUR = 195,
-	LDUR = 197, UR = 192, LUR = 193, LU = 217, LDU = 180, H = 219
-};
+char DR = (char) 218; char LR = (char) 196; char LDR = (char) 194; char LD = (char) 191; char DU = (char) 179; char DUR = (char) 195;
+char LDUR = (char) 197; char UR = (char) 192; char LUR = (char) 193; char LU = (char) 217; char LDU = (char) 180; char H = (char) 219;
 
 class View
 {
 public:
-	View(Picross picross) : game(&picross), displayNum(false) {}	// Constructor for picross
-	View(Rullo rullo) : game(&rullo), displayNum(true) {}			// Constructor for rullo
+	View(Picross *picross) : game(picross), displayNum(false) {}	// Constructor for picross
+	View(Rullo *rullo) : game(rullo), displayNum(true) {}			// Constructor for rullo
 	void playGame();												// While-loop for current game
 	~View() {}														// Destructor
 private:
@@ -41,36 +39,37 @@ void View::playGame()
 		system("CLS");
 		draw();
 		inputHandler();
+		return;
 		cout << endl;
 	}
 }
 
 void View::draw()
 {
-	for (int i = 0; i < game->getRowKeyWidth(); i++) cout << "   "; cout << "    "; for (int i = 1; i <= game->getNumCols(); i++) { if (i <= 9) cout << i << "   "; else cout << i << "  "; } cout << endl;
-	for (int i = 0; i < game->getRowKeyWidth(); i++) cout << "   "; cout << "    " << (char)DR; for (int i = 0; i < game->getNumCols(); i++) { if (i < game->getNumCols() - 1) cout << (char)LR << (char)LR << (char)LDR; else cout << (char)LR << (char)LR << (char)LD << endl; }
+	for (int i = 0; i < game->getRowKeyWidth(); i++) cout << "   "; cout << "    "; for (int i = 1; i <= game->getNumCols(); i++) { if (i <= 9) {formatInt(i); cout << " ";} else {formatInt(i); cout << " "; }} cout << endl;
+	for (int i = 0; i < game->getRowKeyWidth(); i++) cout << "   "; cout << "   " << DR; for (int i = 0; i < game->getNumCols(); i++) { if (i < game->getNumCols() - 1) cout << LR << LR << LDR; else cout << LR << LR << LD; } cout << endl;
 	for (int i = 0; i < game->getColKeyHeight(); i++)
 	{
-		for (int j = 0; j < game->getRowKeyWidth(); j++) cout << "   "; cout << "    " << (char)DU; for (int j = 0; j < game->getNumCols(); j++) { formatInt(game->getColKey(i, j)); if (j < game->getNumCols() - 1) cout << (char)DU; else cout << (char)DU << endl; }
-		if (i < game->getColKeyHeight() - 1) { for (int j = 0; j < game->getRowKeyWidth(); j++) cout << "   "; cout << "  " << (char)DUR; for (int j = 0; j < game->getNumCols(); j++) { if (j < game->getNumCols() - 1) cout << (char)LR << (char)LR << (char)LDUR; else cout << (char)LR << (char)LR << (char)LDU << endl; } }
-		else { for (int j = 0; j < game->getRowKeyWidth(); j++) cout << "   "; cout << "    " << (char)UR; for (int j = 0; j < game->getNumCols(); j++) { if (j < game->getNumCols() - 1) cout << (char)LR << (char)LR << (char)LUR; else cout << (char)LR << (char)LR << (char)LU << endl; } }
+		for (int j = 0; j < game->getRowKeyWidth(); j++) cout << "   "; cout << "   " << DU; for (int j = 0; j < game->getNumCols(); j++) { formatInt(game->getColKey(i, j)); if (j < game->getNumCols() - 1) cout << DU; else cout << DU << endl; }
+		if (i < game->getColKeyHeight() - 1) { for (int j = 0; j < game->getRowKeyWidth(); j++) cout << "   "; cout << "  " << DUR; for (int j = 0; j < game->getNumCols(); j++) { if (j < game->getNumCols() - 1) cout << LR << LR << LDUR; else cout << LR << LR << LDU << endl; } }
+		else { for (int j = 0; j < game->getRowKeyWidth(); j++) cout << "   "; cout << "   " << UR; for (int j = 0; j < game->getNumCols(); j++) { if (j < game->getNumCols() - 1) cout << LR << LR << LUR; else cout << LR << LR << LU << endl; } }
 	}
-	cout << (char)DR; for (int i = 0; i < game->getRowKeyWidth(); i++) { cout << (char)LR << (char)LR; if (i < game->getRowKeyWidth() - 1) cout << (char)LDR; else cout << (char)LD; }
-	cout << (char)DR; for (int i = 0; i < game->getNumCols(); i++) { cout << (char)LR << (char)LR; if (i < game->getNumCols() - 1) cout << (char)LDR; else cout << (char)LD << endl; }
+	cout << "  " << DR; for (int i = 0; i < game->getRowKeyWidth(); i++) { if (i < game->getRowKeyWidth() - 1) cout << LR << LR << LDR; else cout << LR << LR << LD; }
+	cout << DR; for (int i = 0; i < game->getNumCols(); i++) { if (i < game->getNumCols() - 1) cout << LR << LR << LDR; else cout << LR << LR << LD << endl; }
 	char guide = 'A';
 	for (int i = 0; i < game->getNumRows(); i++)
 	{
-		cout << guide++ << " " << (char)DU; for (int j = 0; j < game->getRowKeyWidth(); j++) { formatInt(game->getRowKey(i, j)); cout << (char)DU; }
-		cout << (char)DU; for (int j = 0; j < game->getNumCols(); j++) { formatSpace(i, j); cout << (char)DU; } cout << endl;
+		cout << guide++ << " " << DU; for (int j = 0; j < game->getRowKeyWidth(); j++) { formatInt(game->getRowKey(i, j)); cout << DU; }
+		cout << DU; for (int j = 0; j < game->getNumCols(); j++) { formatSpace(i, j); cout << DU; } cout << endl;
 		if (i < game->getNumRows() - 1)
 		{
-			cout << "  " << (char)DUR; for (int j = 0; j < game->getRowKeyWidth() - 1; j++) cout << (char)LR << (char)LR << (char)LDUR; cout << (char)LR << (char)LR << (char)LDU;
-			cout << (char)DUR; for (int j = 0; j < game->getNumCols() - 1; j++) cout << (char)LR << (char)LR << (char)LDUR; cout << (char)LR << (char)LR << (char)LDU << endl;
+			cout << "  " << DUR; for (int j = 0; j < game->getRowKeyWidth() - 1; j++) cout << LR << LR << LDUR; cout << LR << LR << LDU;
+			cout << DUR; for (int j = 0; j < game->getNumCols() - 1; j++) cout << LR << LR << LDUR; cout << LR << LR << LDU << endl;
 		}
 		else
 		{
-			cout << "  " << (char)UR; for (int i = 0; i < game->getRowKeyWidth(); i++) { cout << (char)LR << (char)LR; if (i < game->getRowKeyWidth() - 1) cout << (char)LUR; else cout << (char)LU; }
-			cout << (char)UR; for (int i = 0; i < game->getNumCols(); i++) { cout << (char)LR << (char)LR; if (i < game->getNumCols() - 1) cout << (char)LUR; else cout << (char)LU << endl; }
+			cout << "  " << UR; for (int i = 0; i < game->getRowKeyWidth(); i++) { cout << LR << LR; if (i < game->getRowKeyWidth() - 1) cout << LUR; else cout << LU; }
+			cout << UR; for (int i = 0; i < game->getNumCols(); i++) { cout << LR << LR; if (i < game->getNumCols() - 1) cout << LUR; else cout << LU << endl; }
 		}
 	}
 }
@@ -84,7 +83,7 @@ void View::formatInt(int integer)
 
 void View::formatSpace(int row, int col)
 {
-	if (game->getBoard(row, col)->boolean) cout << (char)H << (char)H;
+	if (game->getBoard(row, col)->boolean) cout << H << H;
 	else if (displayNum) formatInt(game->getBoard(row, col)->integer);
 	else cout << "  ";
 }
